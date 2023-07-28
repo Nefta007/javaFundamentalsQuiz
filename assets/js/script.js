5b115995456afd69d039f866ae0b9d8f25dd5184
Nefta007 <nperc001@ucr.edu>
2023-07-27 23:44:29 -0700

    sixth commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,3 +16,41 @@ var aButton = document.getElementById("a");
 var bButton = document.getElementById("b");
 var cButton = document.getElementById("c");
 var dButton = document.getElementById("d");
+
+//another way to set up questions based on what I found online
+var myQuestions = [
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        answers : {
+            a: 'Javascript',
+            b: 'terminal/bash',
+            c: 'for loop',
+            d: 'console.log'
+        },
+        correctAnswer: 'b'
+    }
+];
+
+function generateQuiz(question, quizOutline, playersFinalScore, submitBtn, startButton){
+    
+    function showQuestions(question, quizOutline){
+        var output = [];
+        var answers;
+
+        for(var i=0; i<question.length; i++){
+            answers=[];
+            for(letter in question[i].answers){
+                answers.push(
+                    '<label>'+'<input type="radio" name="questions' +i+'"value="'+letter+'">'+letter+': '
+                    + question[i].answers[letter]+'</label>'
+                );
+            }
+            output.push(
+                '<div class="question">' + questions[i].question + '</div>'
+			+ '<div class="answers">' + answers.join('') + '</div>'
+		);
+        }
+        quizContainer.innerHTML = output.join('');
+    }
+}
+//showQuestions(question,quizOutline);
